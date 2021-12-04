b7c9a43c243e6708a0c005f88461d32c33461d6f
Irham <irhamdz@gmail.com>
2021-12-04 22:55:45 +0700

    add new wallet tests

--- tests/test_account.py
+++ tests/test_account.py
@@ -2,7 +2,7 @@ from app.models import Account
 from app.util import generate_token
 
 
-def test_new_account(app):
+def test__new_account(app):
     token = generate_token()
     account = Account(customer_xid='ea0212d3-abd6-406f-8c67-868e814a2436', token=token)
     assert account.customer_xid == 'ea0212d3-abd6-406f-8c67-868e814a2436'
--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -1,6 +1,14 @@
-def test__view_disabled_wallet(client):
-    response = client.get('/api/v1/wallet')
+from app.models import Account
+
+
+def test__missing_token(client):
+    response = client.get('/api/v1/wallet', headers={'Authorization': 'Token iahdiahidhidahiad'})
     assert response.status_code == 403
-    assert b"Welcome to miniwallet!" in response.data
+    assert b"token is missing or not found" in response.data
 
 
+def test__view_balance(client):
+    account = Account.query.first()
+    response = client.get('/api/v1/wallet', headers={"Authorization": f"Token {account.token}"})
+    assert response.status_code == 200
+    assert b"balance" in response.data
